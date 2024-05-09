9ba68302f7b8508f5cbc86ea04258f9800c23566
PabloCoullaut <145350842+PabloCoullaut@users.noreply.github.com>
2024-05-09 11:30:00 +0200

    GET de todas las empresas, PUT

--- InfoEmpresas/src/main/java/com/example/InfoEmpresas/InfoEmpresasController.java
+++ InfoEmpresas/src/main/java/com/example/InfoEmpresas/InfoEmpresasController.java
@@ -1,12 +1,15 @@
 package com.example.InfoEmpresas;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,11 @@ public class InfoEmpresasController {
     public InfoEmpresas crearInfoEmpresas(@RequestBody InfoEmpresas infoEmpresas) {
         return infoEmpresasRepository.save(infoEmpresas);
     }
+    
+    @GetMapping
+    public List<InfoEmpresas> getAllInfoEmpresas() { 
+    	return infoEmpresasRepository.findAll();
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<InfoEmpresas> findInfoEmpresasById(@PathVariable Long id) {
@@ -32,12 +40,59 @@ public class InfoEmpresasController {
             return ResponseEntity.notFound().build();
         }
     }
-    
+
     @GetMapping("/buscar/{nombre}")
     public ResponseEntity<InfoEmpresas> findInfoEmpresasByNombre(@PathVariable String nombre) {
         Optional<InfoEmpresas> empresaOptional = infoEmpresasRepository.findByNombre(nombre);
         if (empresaOptional.isPresent()) {
             return ResponseEntity.ok(empresaOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<InfoEmpresas> actualizarInfoEmpresas(
+            @PathVariable Long id, @RequestBody InfoEmpresas nuevaInfoEmpresas) {
+        Optional<InfoEmpresas> empresaOptional = infoEmpresasRepository.findById(id);
+        if (empresaOptional.isPresent()) {
+            InfoEmpresas empresaExistente = empresaOptional.get();
+        
+            empresaExistente.setNombre(nuevaInfoEmpresas.getNombre());
+            empresaExistente.setDescripcion(nuevaInfoEmpresas.getDescripcion());
+            empresaExistente.setDireccion(nuevaInfoEmpresas.getDireccion());
+            
+            InfoEmpresas empresaActualizada = infoEmpresasRepository.save(empresaExistente);
+            return ResponseEntity.ok(empresaActualizada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PutMapping("/actualizar/{nombre}")
+    public ResponseEntity<InfoEmpresas> actualizarInfoEmpresas(
+            @PathVariable String nombre, @RequestBody InfoEmpresas nuevaInfoEmpresas) {
+        Optional<InfoEmpresas> empresaOptional = infoEmpresasRepository.findByNombre(nombre);
+        if (empresaOptional.isPresent()) {
+            InfoEmpresas empresaExistente = empresaOptional.get();
+        
+            empresaExistente.setNombre(nuevaInfoEmpresas.getNombre());
+            empresaExistente.setDescripcion(nuevaInfoEmpresas.getDescripcion());
+            empresaExistente.setDireccion(nuevaInfoEmpresas.getDireccion());
+            
+            InfoEmpresas empresaActualizada = infoEmpresasRepository.save(empresaExistente);
+            return ResponseEntity.ok(empresaActualizada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteInfoEmpresas(@PathVariable Long id) {
+        Optional<InfoEmpresas> empresaOptional = infoEmpresasRepository.findById(id);
+        if (empresaOptional.isPresent()) {
+            infoEmpresasRepository.deleteById(id);
+            return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.notFound().build();
         }
